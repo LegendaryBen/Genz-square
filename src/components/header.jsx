3a56440885710f8f8cf36fed6a03a33262f6a0cf
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2023-08-01 12:28:27 +0100

    added action to the ham menu and the searchbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import ham from "../images/ham.svg"
 import search from "../images/search.svg";
 import { Link } from 'react-router-dom'
+import { Search } from '../contexts/Searchbar';
+import { Ham } from '../contexts/Hambar';
 
 const Header = () => {
+    const{slideOut} = useContext(Ham);
+    const {down} = useContext(Search);
     return (
         <div className='Header'>
-            <img src={ham}  />
+            <img src={ham} onClick={slideOut}/>
             <span>
                 GEN-Z SQUARE
             </span>
@@ -15,7 +19,7 @@ const Header = () => {
                 <Link to='/' className='sub-children sign-in'>
                     SIGN IN 
                 </Link>
-                <img src={search} alt="" className='sub-children' />
+                <img src={search} alt="" className='sub-children' onClick={down} />
             </div>
         </div>
     )
