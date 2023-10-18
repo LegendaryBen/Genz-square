176c056f50a688b872d1f83f3c1577f87f359d35
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2023-10-18 15:43:55 +0100

    completed the desktop design for the user password screen

--- src/pages/Profile-password.jsx
+++ src/pages/Profile-password.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Profile_headers from '../components/Profile-headers'
+import Profile_inputs from '../components/Profile-inputs'
+import Save_changes from '../components/Save-changes'
+import Submit_btn from '../components/Submit-btn'
+
 
 const Profile_image = () => {
     return (
         <>
-            this is the password
+            <Profile_headers span1='Password' span2='Manage password settings for your account'/>
+            <Profile_inputs label='Current Password' placeholder='Enter your password'/>
+            <Profile_inputs label='New Password' placeholder='Enter your password'/>
+            <Save_changes/>
+            <Submit_btn text="Save Changes"/>
         </>
     )
 }
