1dddc153986b1215c71bb8a837b46eeca56b34da
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2023-12-07 00:13:16 +0000

    made some changes to the user profile page

--- src/components/Navogations.jsx
+++ src/components/Navogations.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import {useState} from 'react'
 import { NavLink } from 'react-router-dom'
-import Arrow from './Arrow'
 import arc from '../images/arc.svg'
 
 const Navogations = () => {
+
+    const[drop,setDrop] = useState(false);
+
     return (
         <>
             <div className='navigations'>
-                <div>
-                    <Arrow image={arc} />
+                <div style={{height:!drop?"auto":"60px",transition:"0.5s"}}>
+                    <img src={arc} alt="" onClick={()=>{setDrop(!drop)}}/>
                     <NavLink to=''>Profile</NavLink><br/>
                     <NavLink to='image' className='nav-special'>Password</NavLink>
                     <NavLink to='email'className='nav-special' >Email Notifications</NavLink>
